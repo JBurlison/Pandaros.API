43e52bf2b9df2b468028e8ed634b66c7fd97528a
Jim Burlison <JBurlison@Gmail.com>
2019-07-24 16:54:44 -0400

    Added ability to set spawn point on servers

---
+++ Pandaros.API/WorldGen/PandarosSpawnPoint.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Pipliz;
+using Pipliz.JSON;
+using static TerrainGeneration.TerrainGenerator;
+
+namespace Pandaros.API.WorldGen
+{
+    [ModLoader.ModManager]
+    public class PandarosSpawnPoint : ISpawnPointProvider
+    {
+        [ModLoader.ModCallback(ModLoader.EModCallbackType.AfterWorldLoad, GameInitializer.NAMESPACE + ".WorldGen.PandarosSpawnPoint.AfterWorldLoad")]
+        public void AfterWorldLoad()
+        {
+            if (ServerManager.TerrainGenerator is TerrainGeneration.TerrainGenerator generator)
+            {
+                var newSpawnPoint = new PandarosSpawnPoint();
+                newSpawnPoint.Default = generator.SpawnPointProvider;
+                generator.SpawnPointProvider = newSpawnPoint;
+            }
+        }
+
+        public ISpawnPointProvider Default { get; set; }
+
+        public Vector3Int GetSpawnPoint()
+        {
+            var locationJson = APIConfiguration.CSModConfiguration.GetorDefault("SpawnLocation", Vector3Int.invalidPos);
+
+            if (locationJson != Vector3Int.invalidPos)
+            {
+                return locationJson;
+            }
+            else
+                return Default.GetSpawnPoint();
+        }
+    }
+}
