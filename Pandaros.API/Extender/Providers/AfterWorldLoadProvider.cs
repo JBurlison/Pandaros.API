325c7c6381f37f7c565f9e8778436bff212a38c4
Jim Burlison <JBurlison@Gmail.com>
2020-08-17 21:47:19 -0400

    Added after world load provider

---
+++ Pandaros.API/Extender/Providers/AfterWorldLoadProvider.cs
@@ -0,0 +1,29 @@
+﻿using Pandaros.API.Items;
+using Pandaros.API.Jobs.Roaming;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Pandaros.API.Extender.Providers
+{
+    public class AfterWorldLoadProvider : IAfterWorldLoadExtender
+    { 
+        public List<Type> LoadedAssembalies { get; } = new List<Type>();
+
+        public string InterfaceName => nameof(IAfterWorldLoad);
+
+        public Type ClassType => null;
+
+
+        public void AfterWorldLoad()
+        {
+            foreach (var s in LoadedAssembalies)
+            {
+                if (Activator.CreateInstance(s) is IAfterWorldLoad modsLoaded)
+                {
+                    modsLoaded.AfterWorldLoad();
+                }
+            }
+        }
+    }
+}
