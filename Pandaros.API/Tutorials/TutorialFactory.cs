d96a8c21f2f0afd1b3b99e42ae7ba00fb9a2bf98
Jim Burlison <JBurlison@Gmail.com>
2020-09-10 15:25:13 -0400

    added button to skip tutorials.

--- Pandaros.API/Tutorials/TutorialFactory.cs
+++ Pandaros.API/Tutorials/TutorialFactory.cs
@@ -1,4 +1,5 @@
-﻿using NetworkUI;
+﻿using ModLoaderInterfaces;
+using NetworkUI;
 using NetworkUI.Items;
 using Pandaros.API.Entities;
 using Pandaros.API.Extender;
@@ -13,7 +14,7 @@ using System.Threading.Tasks;
 
 namespace Pandaros.API.Tutorials
 {
-    public class TutorialFactory : IOnTimedUpdate
+    public class TutorialFactory : IOnTimedUpdate, IOnPlayerPushedNetworkUIButton, IOnPlayerConnectedLate
     {
         static LocalizationHelper _localizationHelper = new LocalizationHelper(GameInitializer.NAMESPACE, "Tutorial");
         public static Dictionary<string, ITutorial> Tutorials { get; set; } = new Dictionary<string, ITutorial>();
@@ -24,11 +25,9 @@ namespace Pandaros.API.Tutorials
         ServerTimeStamp IOnTimedUpdate.NextUpdateTime { get; set; }
 
 
-        [ModLoader.ModCallback(ModLoader.EModCallbackType.OnPlayerConnectedLate, GameInitializer.NAMESPACE + ".Help.TutorialManager.OnPlayerConnectedLate")]
-        public static void OnPlayerConnectedLate(Players.Player p)
+        public void OnPlayerConnectedLate(Players.Player p)
         {
-            var ps = PlayerState.GetPlayerState(p);
-
+            CheckforPlayerTutorials(p);
         }
 
         public static bool TutorialRun(PlayerState playerState, string tutorialName)
@@ -43,23 +42,40 @@ namespace Pandaros.API.Tutorials
 
         public void OnTimedUpdate()
         {
-           foreach (var p in Players.PlayerDatabase.Values.Where(pk => pk.IsConnected()))
+            foreach (var p in Players.PlayerDatabase.Values.Where(pk => pk.IsConnected()))
+                CheckforPlayerTutorials(p);
+        }
+
+        private static void CheckforPlayerTutorials(Players.Player p)
+        {
+            var ps = PlayerState.GetPlayerState(p);
+
+            foreach (var tut in Tutorials.Values)
             {
-                var ps = PlayerState.GetPlayerState(p);
+                bool hasCompleted = false;
 
-                foreach (var tut in Tutorials.Values)
+                if (ps.Tutorials.TryGetValue(tut.Name, out var complete) && complete)
+                    hasCompleted = true;
+
+                if (!hasCompleted && (tut.Prerequisites.Count == 0 || tut.Prerequisites.All(pre => pre.MeetsCondition(p))))
                 {
-                    bool hasCompleted = false;
-
-                    if (ps.Tutorials.TryGetValue(tut.Name, out var complete) && complete)
-                        hasCompleted = true;
-
-                    if (!hasCompleted && tut.Prerequisites.All(pre => pre.MeetsCondition(p)))
-                    {
-                        NetworkMenuManager.SendServerPopup(p, tut.ShowTutorial(p));
-                    }
+                    var menu = tut.ShowTutorial(p);
+                    menu.Items.Add(new ButtonCallback(GameInitializer.NAMESPACE + ".cancelalltorials", new LabelData(_localizationHelper.LocalizeOrDefault("CancelTutorials", p)), 200, 30, ButtonCallback.EOnClickActions.ClosePopup));
+                    NetworkMenuManager.SendServerPopup(p, menu);
+                    break;
                 }
             }
+        }
+
+        public void OnPlayerPushedNetworkUIButton(ButtonPressCallbackData data)
+        {
+            if (data.ButtonIdentifier != GameInitializer.NAMESPACE + ".cancelalltorials")
+                return;
+
+            var ps = PlayerState.GetPlayerState(data.Player);
+
+            foreach (var tut in Tutorials.Keys)
+                ps.Tutorials[tut] = true;
         }
     }
 }
