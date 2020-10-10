f9f5803583365e5ae65f569bded616a83bdc7188
Jim Burlison <JBurlison@Gmail.com>
2020-10-10 12:16:01 -0400

    Fix craft objective.

--- Pandaros.API/Questing/BuiltinObjectives/CraftObjective.cs
+++ Pandaros.API/Questing/BuiltinObjectives/CraftObjective.cs
@@ -1,8 +1,10 @@
-﻿using Newtonsoft.Json;
+﻿using Jobs;
+using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using Pandaros.API.localization;
 using Pandaros.API.Models;
 using Pandaros.API.Questing.Models;
+using Recipes;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,8 +13,28 @@ using System.Threading.Tasks;
 
 namespace Pandaros.API.Questing.BuiltinObjectives
 {
+    [ModLoader.ModManager]
     public class CraftObjective : IPandaQuestObjective
     {
+        public static Dictionary<string, List<CraftObjective>> CraftObjectivesByItem { get; set; } = new Dictionary<string, List<CraftObjective>>();
+
+        [ModLoader.ModCallback(ModLoader.EModCallbackType.OnNPCCraftedRecipe, GameInitializer.NAMESPACE + ".Questing.BuiltinObjectives.OnNPCCraftedRecipe")]
+        public static void OnNPCCraftedRecipe(IJob job, Recipe recipe, List<RecipeResult> results)
+        {
+           foreach (var item in results)
+                if (CraftObjectivesByItem.TryGetValue(ItemId.GetItemId(item.Type), out var craftObjectives))
+                    foreach (var objective in craftObjectives)
+                    {
+                        if (!objective.CurrentCraftCount.TryGetValue(job.Owner.ColonyID, out var count))
+                        {
+                            objective.CurrentCraftCount[job.Owner.ColonyID] = 0;
+                            count = 0;
+                        }
+
+                        objective.CurrentCraftCount[job.Owner.ColonyID] = count + item.Amount;
+                    }
+        }
+
         public CraftObjective(string objectiveKey, string item, int count)
         {
             ObjectiveKey = objectiveKey;
@@ -21,6 +43,15 @@ namespace Pandaros.API.Questing.BuiltinObjectives
 
             if (string.IsNullOrEmpty(LocalizationKey))
                 LocalizationKey = nameof(CraftObjective);
+
+            if (!CraftObjectivesByItem.TryGetValue(Item, out var craftObjectives))
+            {
+                craftObjectives = new List<CraftObjective>();
+                CraftObjectivesByItem[item] = craftObjectives;
+            }
+
+            if (!craftObjectives.Contains(this))
+                craftObjectives.Add(this);
         }
 
         public string ObjectiveKey { get; set; }
@@ -53,7 +84,7 @@ namespace Pandaros.API.Questing.BuiltinObjectives
             else if (CurrentCraftCount[colony.ColonyID] != 0)
                 return CurrentCraftCount[colony.ColonyID] / CraftCount;
             else
-                return 1;
+                return 0;
         }
 
         public void Load(JObject node, IPandaQuest quest, Colony colony)
